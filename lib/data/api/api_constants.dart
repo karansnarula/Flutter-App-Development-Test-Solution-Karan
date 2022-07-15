338de239254c6e7560eee75ce4b6336faf2b7c33
Karan <karansnarula@gmail.com>
2022-07-15 14:58:38 +0700

    New API key added

---
+++ lib/data/api/api_constants.dart
@@ -0,0 +1,7 @@
+class NewsAPI {
+  static const String newsHeadlineURL =
+      'https://google-news1.p.rapidapi.com/topic-headlines';
+  static const String apiKey =
+      'cc1fcaec73msh592f98bc7ded583p115c31jsn1a63390d633c';
+  static const String apiHost = 'google-news1.p.rapidapi.com';
+}
